06f95e3f19eea8f9141f5694e1260c0bab2b137c
xmulh <2678811392@qq.com>
2021-03-30 10:18:58 +0800

    refactor Calculator to use reflect

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public int computer(int num1, int num2, char symbol) {
+    public int computer(int num1, int num2, String symbol) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         Computable computable = ComputeFactory.getCompute(symbol);
         return computable.computer(num1, num2);
     }
